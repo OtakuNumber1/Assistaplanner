bb09366b8ef41979a35912127a93752c3b8bc326
Quentin Pessl <20170061@students.htl-perg.ac.at>
2021-11-28 15:29:55 +0100

    Termine werden jetzt in der Tagesansicht angezeigt (können dort erstellt geändert gelöscht werden

--- Assistaplanner/Tagesansicht.xaml.cs
+++ Assistaplanner/Tagesansicht.xaml.cs
@@ -19,19 +19,104 @@ namespace Assistaplanner
     /// </summary>
     public partial class Tagesansicht : Window
     {
+        private String Wochentag;
         public Tagesansicht(string Wochentag)
         {
             InitializeComponent();
+            this.Wochentag = Wochentag;
+          
             wochentagText.Text = Wochentag;
 
         }
+        private void RenderTermine(string Wochentag)
+        {
+            List<UIElement> delete = new List<UIElement>();
 
- 
+            tagkalender.Children.Clear();
+            tagkalender.Children.Add(tagkalenderGrid);
+
+            List<Termin> termine = SQLiteDataAccess.LoadTermineFromDay(Wochentag);
+            foreach (Termin termin in termine)
+            {
+                Console.WriteLine("1");
+                Label label = erste;
+               
+                if (label != null)
+                {
+                    
+                    Console.WriteLine("Label");
+                    Point point = label.TransformToAncestor(tagkalender).Transform(new Point(0, 0));
+                    double startY = tagkalender.ActualHeight / 26.0 * 2;
+                    double totalHeight = tagkalender.ActualHeight * 24.0 / 26;
+                    int startMinute = termin.vonMinute + 60 * termin.vonStunde;
+                    int bisMinuten = termin.bisMinute + 60 * termin.bisStunde;
+
+                    Button button = new Button();
+                    button.Width = tagkalender.ActualWidth * 250 / (125 + 250 * 7);
+                    button.Height = Math.Max(0, bisMinuten - startMinute) / (24.0 * 60.0) * totalHeight;
+                    button.Margin = new Thickness(point.X, startY + startMinute / (24.0 * 60.0) * totalHeight, 0, 0);
+                    button.Content = termin.TerminTitel;
+                    List<TerminKategorie> kategorien = ShowKategorien.KategorienLaden();
+
+                    Console.WriteLine("Werte:" + button.Width + button.Height + button.Margin);
+                    if (kategorien.Where(k => k.terminKategorieID == termin.TerminKategorie).Count() > 0)
+                    {
+                        Color color = ButtonColour(kategorien.Where(k => k.terminKategorieID == termin.TerminKategorie).First().KategorieFarbe);
+                        button.Background = new SolidColorBrush(color);
+                        Console.WriteLine("3");
+                    }
+                    button.Click += (sender, e) =>
+                    {
+                        TerminBearbeiten terminBearbeiten = new TerminBearbeiten(termin);
+                        terminBearbeiten.ShowDialog();
+                        RenderTermine(Wochentag);
+                    };
+
+                    tagkalender.Children.Add(button);
+                    Panel.SetZIndex(button, 0);
+                }
+            }
+        }
+        private Color ButtonColour(string colourname)
+        {
+            //{ "Blau", "Rot", "Lila", "Hellblau", "Hellgrün", "Gelb", "Grün" };
+            Color color = new Color();
+            switch (colourname)
+            {
+                case "Blau":
+                    color = Color.FromRgb(0, 191, 255);
+                    break;
+                case "Rot":
+                    color = Color.FromRgb(255, 0, 0);
+                    break;
+                case "Lila":
+                    color = Color.FromRgb(191, 62, 255);
+                    break;
+                case "Hellblau":
+                    color = Color.FromRgb(187, 255, 255);
+                    break;
+                case "Hellgrün":
+                    color = Color.FromRgb(0, 255, 127);
+                    break;
+                case "Gelb":
+                    color = Color.FromRgb(255, 255, 0);
+                    break;
+                case "Grün":
+                    color = Color.FromRgb(154, 205, 50);
+                    break;
+            }
+
+            return color;
+        }
+
+
+       
+
         private void terminListeTagesansicht_Click(object sender, RoutedEventArgs e)
         {
             TerminListe termine = new TerminListe();
             termine.ShowDialog();
-            //RenderTermine();
+            RenderTermine(Wochentag);
         }
 
         private void kategorienlisteTagesansicht_Click(object sender, RoutedEventArgs e)
@@ -39,14 +124,14 @@ namespace Assistaplanner
 
             ShowKategorien KategorienListe = new ShowKategorien();
             KategorienListe.ShowDialog();
-            //RenderTermine();
+            RenderTermine(Wochentag);
         }
 
         private void neuerTerminTagesansicht_Click(object sender, RoutedEventArgs e)
         {
             NeuerTermin neuerTermin = new NeuerTermin();
             neuerTermin.ShowDialog();
-            //RenderTermine();
+            RenderTermine(Wochentag);
         }
 
         private void Wochenansicht_Button_Click(object sender, RoutedEventArgs e)
@@ -59,6 +144,11 @@ namespace Assistaplanner
         private void ExitButtonT_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            RenderTermine(Wochentag);
         }
     }
 }
